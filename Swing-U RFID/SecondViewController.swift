1a45309fe7e9f29c5aff811dca29d98a0a7f008a
Hamza Öztürk <hamza.ozturk@busoft.com.tr>
2020-02-29 11:44:16 +0300

    Initial Commit

---
+++ Swing-U RFID/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Swing-U RFID
+//
+//  Created by Hamza Öztürk on 29.02.2020.
+//  Copyright © 2020 Busoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
